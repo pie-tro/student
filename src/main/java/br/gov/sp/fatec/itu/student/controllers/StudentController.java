0640d779cc6c74bc939ab933895fac0129ce98f1
pie.tro <pietromaia28@gmail.com>
2025-05-27 11:11:02 -0300

    cross origin para resolver problema de cors

--- src/main/java/br/gov/sp/fatec/itu/student/controllers/StudentController.java
+++ src/main/java/br/gov/sp/fatec/itu/student/controllers/StudentController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.gov.sp.fatec.itu.student.entities.Student;
 import br.gov.sp.fatec.itu.student.services.StudentService;
-
+@CrossOrigin
 @RestController
 @RequestMapping("students")
 public class StudentController {
