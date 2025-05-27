87aaa563102b542367b38ee59dd7225cb1f71c7b
pie.tro <pietromaia28@gmail.com>
2025-05-27 10:59:11 -0300

    criando endpoints de delete e update em controllers

--- src/main/java/br/gov/sp/fatec/itu/student/controllers/StudentController.java
+++ src/main/java/br/gov/sp/fatec/itu/student/controllers/StudentController.java
@@ -4,8 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,7 +30,15 @@ public class StudentController {
     @PostMapping
     public ResponseEntity<Student> save(@RequestBody Student student){
         return ResponseEntity.created(null).body(service.save(student));
-
     }
-    
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void>delete(@PathVariable long id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+    @PutMapping("{id}")
+    public ResponseEntity<Void> update(@PathVariable long id, @RequestBody Student student){
+        service.update(student, id);
+        return ResponseEntity.noContent().build();
+    }
 }
